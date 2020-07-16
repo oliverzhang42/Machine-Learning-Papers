c65cb8f7c8763dfcf12555a3ce322bda866f0c2d
Oliver <oliver.zhang.42@gmail.com>
2020-07-16 10:46:56 -0700

    Restarting Project

--- utils.py
+++
@@ -1,9 +0,0 @@
-import pickle as pkl
-
-def save(obj, name):
-    with open(name, "wb") as f:
-        pkl.dump(obj, f)
-
-def load(name):
-    with open(name, "rb") as f:
-        return pkl.load(f)
